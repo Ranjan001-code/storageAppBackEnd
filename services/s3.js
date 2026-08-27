e38091c8c702e93d55797a11f8f9f41be8f9d63e
Ranjan001 <mhtcet2022coeprs@gmail.com>
2026-08-27 13:12:24 +0530

    s3 credentials setup

--- services/s3.js
+++ services/s3.js
@@ -8,7 +8,13 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3Client = new S3Client({ region: "ap-south-1" });
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 export const createUploadSignedUrl = async ({ key, contentType }) => {
   const command = new PutObjectCommand({
