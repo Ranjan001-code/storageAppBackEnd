e1bf8d776d92f999dc97d8463fa8ff70373925d1
Ranjan001 <mhtcet2022coeprs@gmail.com>
2026-09-05 18:55:01 +0530

    properly iplemented rateLimiting and setup domain based email service using resend

--- services/sendOtpService.js
+++ services/sendOtpService.js
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import OTP from "../models/otpModel.js";
-import nodemailer from "nodemailer";
-import { nextTick } from "process";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendOtpService(email) {
   const otp = Math.floor(1000 + Math.random() * 9000).toString();
@@ -13,30 +13,47 @@ export async function sendOtpService(email) {
       { upsert: true },
     );
 
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.GMAIL_USER,
+    //     pass: process.env.GMAIL_APP_PASSWORD,
+    //   },
+    // });
+
+    // await transporter.sendMail({
+    //   from: process.env.GMAIL_USER,
+    //   to: email,
+    //   subject: "Storage App OTP",
+    //   html,
+    // });
+
     const html = `
-    <div style="font-family:sans-serif;">
-      <h2>Your OTP is: ${otp}</h2>
-      <p>This OTP is valid for 10 minutes.</p>
-    </div>
-  `;
+  <div style="font-family: Arial, sans-serif; padding: 20px;">
+    <h2>Storify</h2>
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
-      },
-    });
+    <p>Your OTP is:</p>
 
-    await transporter.sendMail({
-      from: process.env.GMAIL_USER,
-      to: email,
-      subject: "Storage App OTP",
+    <h1 style="letter-spacing: 5px;">${otp}</h1>
+
+    <p>This OTP is valid for 10 minutes.</p>
+
+    <p>If you did not request this OTP, you can ignore this email.</p>
+
+    <p>Thanks,<br>Storify Team</p>
+  </div>
+`;
+
+    await resend.emails.send({
+      from: "Storify <noreply@ranjan.website>",
+      to: [email],
+      subject: "Your Storify OTP",
       html,
     });
+
+    return { success: true, message: `OTP sent successfully on ${email}` };
   } catch (err) {
     console.error("OTP sending service failed");
+    return { success: false, message: "Failed to send OTP. Please try again." };
   }
-
-  return { success: true, message: `OTP sent successfully on ${email}` };
 }
